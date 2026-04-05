175a09a57ef2b407d69c270677cef150baea7cfc
Izzatbek <abdusharipovizzat03@gmail.com>
2026-04-05 10:30:27 +0500

    Authentication function has added

--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,13 +1,16 @@
 import { Lock, Mail } from "lucide-react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import type z from "zod";
-import { loginSchema, type loginSchema } from "../schema/auth.schema";
+import { loginSchema  } from "../schema/auth.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "react-toastify";
+import axiosInstance from "../api/axios.instance";
 
 const Auth = () => {
+  const navigate = useNavigate();
 
-  const loginForm = useForm<z.input<typeof loginSchema>>({
+  const {register,handleSubmit,formState: {errors}} = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
       email: "",
@@ -15,37 +18,58 @@ const Auth = () => {
     }
   })
 
+  const onSubmit = async (values:z.infer<typeof loginSchema>) => {
+    try {
+      const {data} = await axiosInstance.post("/auth/login",values);
+      localStorage.setItem("accessToken",data.accessToken);
+      toast.success("Successfully");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      localStorage.removeItem("accessToken");
+      toast.error("Authentication failed")
+    }
+  }
+
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
       <div className="w-full max-w-lg bg-white shadow-lg rounded-md p-6">
         <h1 className="text-2xl font-semibold py-4">Sign In Now</h1>
-        <form className="space-y-6">
-          <div className="flex items-center gap-2 border  p-2 rounded-md">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2 border  p-2 rounded-md">
             <Mail />
             <input
               type="email"
+              {...register("email")}
               placeholder="example@gmail.com"
               className="outline-none w-full"
             />
           </div>
-          <div className="flex items-center gap-2 border p-2 rounded-md">
+          {errors.email && <span className="text-xs text-red-500">{errors.email.message}</span>}
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2 border p-2 rounded-md">
             <Lock />
             <input
+              {...register("password")}
               type="password"
               placeholder="********"
               className="outline-none w-full"
             />
           </div>
+          {errors.password && <span className="text-xs text-red-500">{errors.password.message}</span>}
+          </div>
           <button
             type="submit"
             className="w-full bg-black text-white p-3 rounded-md cursor-pointer hover:bg-gray-800
-           transition dur3"
+           transition duration-300"
           >
             Sign In Now
           </button>
         </form>
         <div className="pt-4">
-              <p>Don't have an account? <Link to={'/register'} className="hover: underline font-bold">Sign Up</Link></p>
+              <p>Don't have an account? <Link to={'/register'} className="hover:underline font-bold">Sign Up</Link></p>
         </div>
       </div>
     </div>
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,67 +1,127 @@
-import { Lock, Mail, UserRound } from "lucide-react"
-import { Link } from "react-router-dom"
-import z from "zod"
-import { registerSchema } from "../schema/auth.schema"
-import { useForm } from 'react-hook-form'
-import { zodResolver } from "@hookform/resolvers/zod"
+import { Lock, Mail, UserRound } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import z from "zod";
+import { registerSchema } from "../schema/auth.schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "react-toastify";
+import axiosInstance from "../api/axios.instance";
 
 const Register = () => {
+  const navigate = useNavigate();
 
-    const registerForm = useForm<z.input<typeof registerSchema>>({
-        resolver: zodResolver(registerSchema),
-        defaultValues: {
-          firstname: "",
-          lastname: "",
-          username: "",
-          email: "",
-          password: ""
-        }
-    })
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof registerSchema>>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      firstname: "",
+      lastname: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof registerSchema>) => {
+    try {
+      const { data } = await axiosInstance.post("/auth/register", values);
+      localStorage.setItem("accessToken", data.accessToken);
+      toast.success("Successfully");
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      localStorage.removeItem("accessToken");
+      toast.error("Registration failed");
+    }
+  };
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
       <div className="w-full max-w-lg bg-white shadow-lg rounded-md p-6">
         <h1 className="text-2xl font-semibold py-4">Sign Up Now</h1>
-        <form className="space-y-6">
-              <div className="flex items-center gap-2 border p-2 rounded-md">
-                <UserRound />
-                <input
-                  type="text"
-                  placeholder="Firstname"
-                  className="outline-none w-full"
-                />
-              </div>
-              <div className="flex items-center gap-2 border  p-2 rounded-md">
-                <UserRound />
-                <input
-                  type="text"
-                  placeholder="Lastname"
-                  className="outline-none w-full"
-                />
-              </div>
-              <div className="flex items-center gap-2 border  p-2 rounded-md">
-                <UserRound />
-                <input
-                  type="text"
-                  placeholder="Username"
-                  className="outline-none w-full"
-                />
-              </div>
-          <div className="flex items-center gap-2 border  p-2 rounded-md">
-            <Mail />
-            <input
-              type="email"
-              placeholder="example@gmail.com"
-              className="outline-none w-full"
-            />
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2 border p-2 rounded-md">
+              <UserRound />
+              <input
+                {...register("firstname")}
+                type="text"
+                placeholder="Firstname"
+                className="outline-none w-full"
+              />
+            </div>
+            {errors.firstname && (
+              <span className="text-xs text-red-500">
+                {errors.firstname.message}
+              </span>
+            )}
           </div>
-          <div className="flex items-center gap-2 border p-2 rounded-md">
-            <Lock />
-            <input
-              type="password"
-              placeholder="********"
-              className="outline-none w-full"
-            />
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2 border  p-2 rounded-md">
+              <UserRound />
+              <input
+                {...register("lastname")}
+                type="text"
+                placeholder="Lastname"
+                className="outline-none w-full"
+              />
+            </div>
+            {errors.lastname && (
+              <span className="text-xs text-red-500">
+                {errors.lastname.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2 border  p-2 rounded-md">
+              <UserRound />
+              <input
+                {...register("username")}
+                type="text"
+                placeholder="Username"
+                className="outline-none w-full"
+              />
+            </div>
+            {errors.username && (
+              <span className="text-xs text-red-500">
+                {errors.username.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2 border  p-2 rounded-md">
+              <Mail />
+              <input
+                {...register("email")}
+                type="email"
+                placeholder="example@gmail.com"
+                className="outline-none w-full"
+              />
+            </div>
+            {errors.email && (
+              <span className="text-xs text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center gap-2 border p-2 rounded-md">
+              <Lock />
+              <input
+                {...register("password")}
+                type="password"
+                placeholder="********"
+                className="outline-none w-full"
+              />
+            </div>
+            {errors.password && (
+              <span className="text-xs text-red-500">
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <button
             type="submit"
@@ -72,11 +132,19 @@ const Register = () => {
           </button>
         </form>
         <div className="pt-4">
-              <p>Already have an account? <Link to={"/auth"} className="font-bold cursor-pointer hover:underline">Sign In</Link></p>
+          <p>
+            Already have an account?{" "}
+            <Link
+              to={"/auth"}
+              className="font-bold cursor-pointer hover:underline"
+            >
+              Sign In
+            </Link>
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
